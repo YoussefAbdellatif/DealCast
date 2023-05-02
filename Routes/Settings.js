13136da3d0414008f7d7cb9982d9778bcb9c946e
DR <youssef.gad.cs@gmail.com>
2023-05-02 05:49:16 +0300

    first Commit

---
+++ Routes/Settings.js
@@ -0,0 +1,40 @@
+
+
+/*  Get Functions From Controllers */
+
+/*
+const {     deletePost,
+        getPostById, 
+        getAllPosts,
+         pushPost,
+         updatePost } = require('../Controllers/postController');
+*/
+
+
+/* Require Router and Use It
+const express = require('express');
+const router = express.Router();
+*/
+
+/* Handle Upcoming Requests */ 
+
+/*
+router.get('/:id', (req, res) => {
+    getPostById(req,res)
+});
+router.get('/', (req, res) => {
+getAllPosts(req, res)
+});
+router.post('/', (req, res) => {
+    pushPost(req, res)
+})
+router.delete('/:id',(req,res)=>{
+    deletePost(req,res)})
+    
+router.put('/:id',(req,res)=>{
+    updatePost(req,res)})
+*/
+
+/* Export the Router
+module.exports = router;
+*/
